9b669fec18f51be235cebffc92d979bb4b0bc339
adrian.llonor <adrianllonor@gmail.com>
2024-02-18 12:56:48 -0700

    Hover to view full job desc.

--- client/src/components/jobPosting/JobListing.js
+++ client/src/components/jobPosting/JobListing.js
@@ -8,9 +8,23 @@ const JobListing = () => {
         <div class="card w-100">
         <div class="card-body">
             <h3 class="card-title">Job Position</h3> 
-            <h5>Company</h5>
+            <h5>Company | Location</h5>
             <div class="description">
-                <p class="card-text">Job Description</p>
+                <p class="card-text">Summary of Job Application</p>
+                <p class="card-hover-text">
+                    Full Job Application Example:
+                    <p></p> 
+                    Strong understanding or Snowflake on Azure Architecture, design, implementation and operationalization of large-scale data and analytics solutions on Snowflake Cloud Data Warehouse.
+                    Hands-on development experience with Snowflake features such as Snow SQL; Snow Pipe; Python; Tasks; Streams; Time travel; Zero Copy Cloning; Optimizer; Metadata Manager; data sharing; and stored procedures.
+                    Experience in Data warehousing - OLTP, OLAP, Dimensions, Facts, and Data modeling.
+                    Need to have working knowledge of MS Azure configuration items with respect to Snowflake.
+                    Developing EL pipelines in and out of data warehouse using combination of Data bricks, Python and Snow SQL.
+                    Developing scripts UNIX, Python etc. to Extract, Load and Transform data, as well as other utility functions.
+                    Provide production support for Data Warehouse issues such data load problems, transformation translation problems
+                    Translate mapping specifications to data transformation design and development strategies and code, incorporating standards and best practices for optimal execution.
+                    Understanding data pipelines and modern ways of automating data pipeline using cloud based testing and clearly document implementations, so others can easily understand the requirements, implementation, and test conditions.
+                    Perform code reviews to ensure fit to requirements, optimal execution patterns and adherence to established standards.
+                </p>
             </div>
             <button class="btn btn-primary" type="submit">Apply</button>
         </div>
