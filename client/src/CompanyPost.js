0a975f84e84f70809831de459fc190899d886e00
MichelleL <michelle.loi@ucalgary.ca>
2024-02-18 04:45:20 -0700

    added submit button

--- client/src/CompanyPost.js
+++ client/src/CompanyPost.js
@@ -9,6 +9,7 @@ const CompanyPost = () => {
         <div className="container company-post-page">
             <form>
                 <h1>Create A New Job Listing</h1>
+                <p className="instruction-text">Fill the form below then click submit</p>
 
                 <div>
                     <label htmlFor="companyName" className="form-label">Company Name</label>
@@ -17,9 +18,15 @@ const CompanyPost = () => {
                 </div>
 
                 <div>
-                    <label htmlFor="position" className="form-label">Position</label>
-                    <input type="text" className="form-control" id="position"/>
+                    <label htmlFor="job-title" className="form-label">Job Title</label>
+                    <input type="text" className="form-control" id="job-title"/>
                 </div>
+
+                <div>
+                    <label htmlFor="job-type" className="form-label">Job Type</label>
+                    <input type="text" className="form-control" id="job-type" placeholder="Part-time, Full-time"/>
+                </div>
+
 
                 <div>
                     <label htmlFor="location" className="form-label">Location</label>
@@ -39,6 +46,7 @@ const CompanyPost = () => {
                 <label className="description-label">Description</label>
                 <RichTextEditor/>
             </form>
+            <button type="button" className="btn btn-primary submit-btn">Submit</button>
         </div>
         )
 }
