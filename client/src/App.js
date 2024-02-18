e0b170b6014232485ca88e748543c741a093bb06
Ryan <ryan.loi@ucalgary.ca>
2024-02-17 22:26:01 -0700

    created a new nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -82,9 +82,6 @@ function App() {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <Link className="nav-link" to="/">Home</Link>
-                </li>
-                <li className="nav-item">
                   <Link className="nav-link" to="/signup">Signup</Link>
                 </li>
                 <li className="nav-item">
