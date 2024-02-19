e3ba0a261dac15249a7f13efc7d4fdc1b472e707
MichelleL <michelle.loi@ucalgary.ca>
2024-02-18 17:19:41 -0700

    fixed responsiveness of text editor

--- client/src/CompanyPost.js
+++ client/src/CompanyPost.js
@@ -7,43 +7,33 @@ const CompanyPost = () => {
 
     return (
         <div className="container company-post-page">
-            <form className="row">
-                <div className="col">
+            <form className="company-post-row">
+                <div className="company-post-col">
+                    <h1 className="post-heading">Create A New Job Listing</h1>
+                    <p className="instruction-text">Fill the form below then click submit</p>
 
-                </div>
-                <h1 className="post-heading">Create A New Job Listing</h1>
-                <p className="instruction-text">Fill the form below then click submit</p>
-
-                <div>
                     <label htmlFor="job-title" className="form-label">Job Title</label>
                     <input type="text" className="form-control" id="job-title"/>
-                </div>
 
-                <div>
                     <label htmlFor="job-type" className="form-label">Job Type</label>
                     <input type="text" className="form-control" id="job-type" placeholder="Part-time, Full-time"/>
-                </div>
 
-                <div>
                     <label htmlFor="location" className="form-label">Location</label>
                     <input type="text" className="form-control" id="location"/>
-                </div>
 
-                <div>
                     <label htmlFor="salary" className="form-label">Salary</label>
                     <input type="text" className="form-control" id="salary"/>
-                </div>
 
-                <div>
                     <label htmlFor="contact" className="form-label">Contact Information</label>
                     <input type="text" className="form-control" id="contact"/>
+
+                    <label className="description-label">Description</label>
+                    <RichTextEditor/>
+
+                    <div className="submit-btn-container">
+                        <button type="button" className="btn btn-primary submit-btn">Submit</button>
+                    </div>
                 </div>
-
-                <label className="description-label">Description</label>
-
-                <RichTextEditor/>
-
-                <button type="button" className="btn btn-primary submit-btn">Submit</button>
             </form>
         </div>
         )
