9515baf810a8126b30c82a176eadadd050cd1207
adrian.llonor <adrianllonor@gmail.com>
2024-02-19 15:56:02 -0700

    Beginning filter functionality

--- client/src/JobBoard.js
+++ client/src/JobBoard.js
@@ -2,22 +2,24 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import JobListing from "./components/jobPosting/JobListing";
-
+import { Button } from 'react-bootstrap';
 
 function JobBoard() {
+  
   return (
     <div className="page-container">
       <h1 className="page-title">Relevant Jobs:</h1>
+      {/* <Button type="button" class="btn btn-dark" id="filter"> Test Filter </Button> */}
       <p/>
       <div className="content">
         <div class="card-columns" >
-             {/* How to pass in Job Listings for the time being until backend can get this from Job Posting*/}
-            <JobListing position="Junior Software Engineer" company="Amazon" location="Vancouver, BC" description="20+ Years of experience doing AI and ML"/>
+            <JobListing position="Junior Software Engineer" company="Amazon" location="Vancouver, BC" 
+              description="20+ Years of experience doing AI and ML"/>
             <JobListing/>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default JobBoard;
