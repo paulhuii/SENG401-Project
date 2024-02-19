3c195d7e4bea3592e1b87fb3973f63ac62a690d6
Ryan <ryan.loi@ucalgary.ca>
2024-02-18 23:36:37 -0700

    finalized the card carousel

--- client/src/Home.js
+++ client/src/Home.js
@@ -7,62 +7,127 @@ import {Button, Card} from "react-bootstrap";
 
 
 function Home() {
-  return (
-    <div className="home-page-container">
-        <h1 className="home-page-slogan">Propel Your Career into the Future with Job Hub</h1>
+    return (
+        <div className="home-page-container">
+            <h1 className="home-page-slogan">Propel Your Career or Business into the Future with Job Hub</h1>
 
-        <div className="Home-page-content">
-            <Carousel interval={6000}>
-                <Carousel.Item>
-                    <div className="home-carousel-card-container">
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="holder.js/100px180" />
-                            <Card.Body>
-                                <Card.Title className="home-card-title">Card Title</Card.Title>
-                                <Card.Text className="home-card-text">
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
+            <div className="Home-page-content">
+                <Carousel className="home-carousel" interval={6000}>
+                    <Carousel.Item>
+                        <div className="home-carousel-card-container">
+                            <Card>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title className="home-card-title">Job 1</Card.Title>
+                                    <Card.Text className="home-card-text">
+                                        Job 1 and some info about the jobs
+                                    </Card.Text>
+                                    <Button variant="primary" className="home-card-button">Go to Applicant Profile</Button>
+                                </Card.Body>
+                            </Card>
 
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="holder.js/100px180" />
-                            <Card.Body>
-                                <Card.Title className="home-card-title">Card Title</Card.Title>
-                                <Card.Text className="home-card-text">
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
+                            <Card>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title className="home-card-title">Job Applicant 2</Card.Title>
+                                    <Card.Text className="home-card-text">
+                                        Job Applicant 2 and some info about then
+                                    </Card.Text>
+                                    <Button variant="primary" className="home-card-button">Go to Applicant Profile</Button>
+                                </Card.Body>
+                            </Card>
 
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="holder.js/100px180" />
-                            <Card.Body>
-                                <Card.Title className="home-card-title">Card Title</Card.Title>
-                                <Card.Text className="home-card-text">
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </div>
-                </Carousel.Item>
+                            <Card>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title className="home-card-title">Job Applicant 3</Card.Title>
+                                    <Card.Text className="home-card-text">
+                                        Job Applicant 3 and some info about then
+                                    </Card.Text>
+                                    <Button variant="primary" className="home-card-button">Go to Applicant Profile</Button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </Carousel.Item>
 
+                    <Carousel.Item>
+                        <div className="home-carousel-card-container">
+                            <Card>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title className="home-card-title">Job Applicant 1</Card.Title>
+                                    <Card.Text className="home-card-text">
+                                        Job Applicant 1 and some info about then
+                                    </Card.Text>
+                                    <Button variant="primary" className="home-card-button">Go to Applicant Profile</Button>
+                                </Card.Body>
+                            </Card>
 
-            </Carousel>
+                            <Card>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title className="home-card-title">Job Applicant 2</Card.Title>
+                                    <Card.Text className="home-card-text">
+                                        Job Applicant 2 and some info about then
+                                    </Card.Text>
+                                    <Button variant="primary" className="home-card-button">Go to Applicant Profile</Button>
+                                </Card.Body>
+                            </Card>
+
+                            <Card>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title className="home-card-title">Job Applicant 3</Card.Title>
+                                    <Card.Text className="home-card-text">
+                                        Job Applicant 3 and some info about then
+                                    </Card.Text>
+                                    <Button variant="primary" className="home-card-button">Go to Applicant Profile</Button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                        <div className="home-carousel-card-container">
+                            <Card>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title className="home-card-title">Company 1</Card.Title>
+                                    <Card.Text className="home-card-text">
+                                        Company 1 and info about it/job positions available
+                                    </Card.Text>
+                                    <Button variant="primary" className="home-card-button">Go to Company Profile</Button>
+                                </Card.Body>
+                            </Card>
+
+                            <Card>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title className="home-card-title">Company 2</Card.Title>
+                                    <Card.Text className="home-card-text">
+                                        Company 2 and info about it/job positions available
+                                    </Card.Text>
+                                    <Button variant="primary" className="home-card-button">Go to Company Profile</Button>
+                                </Card.Body>
+                            </Card>
+
+                            <Card>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title className="home-card-title">Company 3</Card.Title>
+                                    <Card.Text className="home-card-text">
+                                        Company 3 and info about it/job positions available
+                                    </Card.Text>
+                                    <Button variant="primary" className="home-card-button">Go to Company Profile</Button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
 
         </div>
-
-        <footer className="Home-Page-Footer"> test</footer>
-
-
-    </div>
-  );
+    );
 }
 
 export default Home;
