c4c7dc4d088cc6a92028a47f4deda5f22f79c7fd
adrian.llonor <adrianllonor@gmail.com>
2024-02-29 19:08:17 -0700

    Quick renaming fix to .card

--- client/src/components/jobPosting/JobListing.js
+++ client/src/components/jobPosting/JobListing.js
@@ -9,13 +9,13 @@ const JobListing = (props) => {
 
     return(
     <div class="row-lg-4 d-flex align-items-stretch">
-        <div class="card w-100">
-        <div class="card-body">
-            <h3 class="card-title">{props.position ? props.position : "No position given"}</h3> 
+        <div class="jlcard w-100">
+        <div class="jlcard-body">
+            <h3 class="jlcard-title">{props.position ? props.position : "No position given"}</h3> 
             <h5>{props.company ? props.company : "No company given"} | <span class="text-muted">{props.location ? props.location : "No location given"}</span> </h5>
             <div class="description">
-                <p class="card-text">{props.description ? String(props.description).slice(0,256) + "..." : "No description available"}</p>
-                <p class="card-hover-text">{props.description ? props.description : "No description available"}</p>
+                <p class="jlcard-text">{props.description ? String(props.description).slice(0,256) + "..." : "No description available"}</p>
+                <p class="jlcard-hover-text">{props.description ? props.description : "No description available"}</p>
             </div>
             <button class="btn btn-primary p-2" type="submit">Apply</button> 
             {/* May have to alter logic if we are accounting for hours as well */}
