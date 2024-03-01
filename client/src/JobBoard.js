71937933e8736886eb15c44aa061a1400bfed498
adrian.llonor <adrianllonor@gmail.com>
2024-02-29 19:47:17 -0700

    Fixed wide page bug

--- client/src/JobBoard.js
+++ client/src/JobBoard.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import JobListing from "./components/jobPosting/JobListing";
 import { Button } from 'react-bootstrap';
+import "./JobBoard.css"
 
 function JobBoard() {
   
   return (
-    <div className="page-container">
-      <h1 className="page-title">Relevant Jobs:</h1>
+    <div className="jbpage-container">
+      <h1 className="page-title">Available Jobs:</h1>
       {/* <Button type="button" class="btn btn-dark" id="filter"> Test Filter </Button> */}
       <p/>
       <div className="content">
