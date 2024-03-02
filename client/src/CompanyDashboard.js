694ce8e65eed0964ff1e4a008d139385a4c2de74
MichelleL <michelle.loi@ucalgary.ca>
2024-03-01 17:38:52 -0700

    fixed company dashboard pages

--- client/src/CompanyDashboard.js
+++ client/src/CompanyDashboard.js
@@ -6,41 +6,45 @@ import "./CompanyDashboard.css"
 
 const CompanyDashboard = () => {
   return (
-      <div className="container-fluid company-dashboard-page">
-          <div className="row mt-3 mb-3 p-2 text-center company-dash-header">
-              <div className="col">
+      <div className="container-fluid">
+          <div className="row mt-3 mb-1 p-2 company-dash-header">
+              <div className="col-6">
                   <h1>Company Dashboard</h1>
+              </div>
+
+              <div className="col-6 d-flex justify-content-end align-items-center">
+                  <a href="/CompanyPost" className="btn btn-primary">Post a New Job</a>
               </div>
           </div>
 
-          <Tab.Container id="company-dash-menu" defaultActiveKey="company_profile">
-              <Row>
-                  <Col sm={2}>
-                      <a href="/CompanyPost" className="btn btn-primary mb-3 company-post-btn">Post a New Job</a>
+          <div className="row">
+              <Tab.Container id="company-dash-menu" defaultActiveKey="company_profile">
+                  <Row>
+                      <Col lg={2} md={3}>
+                          <Nav variant="pills" className="flex-column">
+                              <Nav.Item>
+                                  <Nav.Link eventKey="company_profile">Company Profile</Nav.Link>
+                              </Nav.Item>
 
-                      <Nav variant="pills" className="flex-column company-dashboard-pills">
-                          <Nav.Item>
-                              <Nav.Link eventKey="company_profile">Company Profile</Nav.Link>
-                          </Nav.Item>
+                              <Nav.Item>
+                                  <Nav.Link eventKey="job_listings">Job Listings</Nav.Link>
+                              </Nav.Item>
+                          </Nav>
+                      </Col>
+                      <Col lg={10} md={9}>
+                          <Tab.Content>
+                              <Tab.Pane eventKey="company_profile">
+                                  <h1>Company Profile</h1>
+                              </Tab.Pane>
 
-                          <Nav.Item>
-                              <Nav.Link eventKey="job_listings">Job Listings</Nav.Link>
-                          </Nav.Item>
-                      </Nav>
-                  </Col>
-                  <Col sm={10}>
-                      <Tab.Content>
-                          <Tab.Pane eventKey="company_profile">
-                              <h1>Company Profile</h1>
-                          </Tab.Pane>
-
-                          <Tab.Pane eventKey="job_listings">
-                              <h1>Job Listings</h1>
-                          </Tab.Pane>
-                      </Tab.Content>
-                  </Col>
-              </Row>
-          </Tab.Container>
+                              <Tab.Pane eventKey="job_listings">
+                                  <h1>Job Listings</h1>
+                              </Tab.Pane>
+                          </Tab.Content>
+                      </Col>
+                  </Row>
+              </Tab.Container>
+          </div>
       </div>
   )
 }
--- client/src/CompanyPost.js
+++ client/src/CompanyPost.js
@@ -6,11 +6,11 @@ import RichTextEditor from "./components/TextEditor/RichTextEditor";
 const CompanyPost = () => {
 
     return (
-        <div className="container-fluid shadow mt-4 mb-4 rounded p-3 company-post-page">
-            <form className="row company-post-row">
-                <div className="col company-post-col">
-                    <h1 className="post-heading">Create A New Job Listing</h1>
-                    <p className="instruction-text">Fill the form below then click submit</p>
+        <div className="container-fluid d-flex justify-content-center align-items-center company-post-pg">
+            <form className="row shadow mt-3 mb-3 p-3 rounded bg-white company-post-form">
+                <div className="col">
+                    <h1 className="post-heading text-center">Create A New Job Listing</h1>
+                    <p className="instruction-text text-center">Fill the form below then click submit</p>
 
                     <label htmlFor="job-title" className="form-label">Job Title</label>
                     <input type="text" className="form-control" id="job-title"/>
@@ -27,10 +27,10 @@ const CompanyPost = () => {
                     <label htmlFor="contact" className="form-label">Contact Information</label>
                     <input type="text" className="form-control" id="contact"/>
 
-                    <label className="description-label">Description</label>
+                    <label className="description-label mb-2">Description</label>
                     <RichTextEditor/>
 
-                    <div className="submit-btn-container">
+                    <div className="d-flex justify-content-center align-items-center pt-3">
                         <button type="button" className="btn btn-primary post-btns">Submit</button>
                         <a href="/CompanyDashboard" className="btn btn-primary post-btns">Cancel</a>
                     </div>
