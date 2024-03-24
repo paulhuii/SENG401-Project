d9f1813c91257c4b3d447f2a9fd28699bfb1e24c
adrian.llonor <adrianllonor@gmail.com>
2024-03-23 21:04:24 -0600

    commented out dummy test

--- client/src/__tests__/ViewApplicants.test.js
+++ client/src/__tests__/ViewApplicants.test.js
@@ -1,15 +1,15 @@
-import { render, screen } from "@testing-library/react";
+// import { render, screen } from "@testing-library/react";
 
-import ViewApplicants from "../components/ViewApplicants/ViewApplicants";
+// import ViewApplicants from "../components/ViewApplicants/ViewApplicants";
 
-describe("ViewApplicants", () => {
-  /* Test block component: */
-  it("should have gender text", () => {
-    /* Render what we want to test */
-    render(<ViewApplicants />);
+// describe("ViewApplicants", () => {
+//   /* Test block component: */
+//   it("should have gender text", () => {
+//     /* Render what we want to test */
+//     render(<ViewApplicants />);
 
-    const gender = screen.getByText("Gender: Female");
-    expect(gender).toBeInTheDocument();
+//     const gender = screen.getByText("Gender: Female");
+//     expect(gender).toBeInTheDocument();
 
-  });
-});
+//   });
+// });
