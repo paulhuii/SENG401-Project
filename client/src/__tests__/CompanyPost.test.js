8695d6b01d942d268765223beeb5ed8501f2371a
MichelleL <michelle.loi@ucalgary.ca>
2024-03-24 23:15:46 -0600

    created tests for companypost page

---
+++ client/src/__tests__/CompanyPost.test.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import {render, screen, waitFor, within} from '@testing-library/react';
+import CompanyPost from "../CompanyPost";
+import { BrowserRouter as Router } from 'react-router-dom';
+import user from '@testing-library/user-event'
+
+const dummyJobData = {
+    title: 'Software Engineer',
+    jobType: 'Full-time',
+    location: 'Canada',
+    salary: '100,000',
+    contact: 'James@company.com',
+    description: 'Software engineer role',
+};
+
+function getJobTitle() {
+    return screen.getByRole('textbox', {
+        name: /Job Title/i
+    });
+}
+
+function getJobType(jobType) {
+    const dropdown = screen.getByRole('combobox', { name: /Job Type/i});
+    user.selectOptions(
+        dropdown,
+        within(dropdown).getByRole('option', {name: jobType})
+    );
+}
+
+function getLocation() {
+    return screen.getByRole('textbox', {
+        name: /Location/i
+    });
+}
+
+function getSalary() {
+    return screen.getByRole('textbox', {
+        name: /Salary/i
+    });
+}
+
+function getContactInfo() {
+    return screen.getByRole('textbox', {
+        name: /Contact Information/i
+    });
+}
+
+function getDescription() {
+    return screen.getByRole('textbox', {
+        name: /description/i
+    })
+}
+
+// Mock react router dom useNavigate
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: jest.fn(),
+}));
+
+describe('CompanyPost', () => {
+    beforeEach(() => {
+        // eslint-disable-next-line testing-library/no-render-in-setup
+        render(
+            <Router>
+                <CompanyPost/>
+            </Router>
+        );
+    });
+
+    it('onSubmit when all correct fields are entered should pass', async () => {
+
+        const navigate = jest.fn();
+        jest.spyOn(require('react-router-dom'), 'useNavigate').mockReturnValue(navigate);
+
+        global.fetch = jest.fn().mockResolvedValue({
+            ok: true,
+            json: () => Promise.resolve(JSON.stringify(dummyJobData)),
+        });
+
+        user.type(getJobTitle(), dummyJobData.title)
+        getJobType(dummyJobData.jobType)
+        user.type(getLocation(), dummyJobData.location)
+        user.type(getSalary(), dummyJobData.salary)
+        user.type(getContactInfo(), dummyJobData.contact)
+        user.type(getDescription(), dummyJobData.description)
+
+        user.click(screen.getByRole('button', { name: /Submit/i }));
+
+        await waitFor(() => expect(navigate).toHaveBeenCalledWith('/CompanyDashboard', {
+            state: {
+                jobPosted: true,
+                jobData: JSON.stringify(dummyJobData),
+            }
+        }));
+    });
+});
