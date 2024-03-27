29806488ad9ad777e243b14128f82325d3ab18c4
alphaboy27 <rafaeghumman01@gmail.com>
2024-03-27 08:35:26 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,14 +11,14 @@
  */
 
 // Import required modules
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const authRoutes = require('./routes/authRoutes'); // Import the authentication routes
-const userRoutes = require('./routes/userRoutes'); // Import the user routes
-const jobRoutes = require('./routes/jobRoutes')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes"); // Import the authentication routes
+const userRoutes = require("./routes/userRoutes"); // Import the user routes
+const jobRoutes = require("./routes/jobRoutes");
 
-require('dotenv').config({ path: '../.env' });
+require("dotenv").config({ path: "../.env" });
 
 // Create an Express application
 const app = express();
@@ -26,16 +26,13 @@ const app = express();
 // Configure middleware
 app.use(
   cors({
-    origin: [
-      "https://jobhub.company",
-       "https://www.jobhub.company"
-    ],
-    credentials: true,
-  })); // Enable Cross-Origin Resource Sharing (CORS)
+    origin: "*"
+  })
+); // Enable Cross-Origin Resource Sharing (CORS)
 app.use(express.json()); // Parse incoming request bodies as JSON
 
 // Middleware to serve static files from 'uploads' directory
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
 // Middleware to log each incoming request
 app.use((req, res, next) => {
@@ -45,24 +42,23 @@ app.use((req, res, next) => {
 
 // Connect to MongoDB
 
-mongoose.connect(process.env.MONGO, {
-}).then(() => console.log('Universal connection to MongoDB established'))
-    .catch(err => console.error('Connection error:', err));
-
+mongoose
+  .connect(process.env.MONGO, {})
+  .then(() => console.log("Universal connection to MongoDB established"))
+  .catch((err) => console.error("Connection error:", err));
 
 const db = mongoose.createConnection(process.env.MONGO);
 
 //Catch connection error for db
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB db');
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB db");
 });
 
-
 // Use the authentication routes
-app.use('/api', authRoutes);
-app.use('/api', userRoutes);
-app.use('/api/jobs', jobRoutes);
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api/jobs", jobRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 5000; // Default port is 5000 if not specified in environment
