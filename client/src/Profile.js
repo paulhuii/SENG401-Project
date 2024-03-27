45ffdaff9f9bb9fdbb12700fa7609b3d2e3ee113
alphaboy27 <rafaeghumman01@gmail.com>
2024-03-27 09:34:51 -0600

    url update

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -31,7 +31,7 @@ function Profile() {
         return;
       }
   
-      const response = await fetch('/api/profile/info', {
+      const response = await fetch('https://backend.jobhub.company/api/profile/info', {
         method: 'GET',
         headers: {
           'Authorization': `Bearer ${token}`, // Include the authentication token
@@ -65,7 +65,7 @@ function Profile() {
       const token = localStorage.getItem('token');
       console.log('Sending request to /api/profile with data:', updatedData);
       console.log('Authorization Token:', token);
-      const response = await fetch('/api/profile', {
+      const response = await fetch('https://backend.jobhub.company/api/profile', {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
